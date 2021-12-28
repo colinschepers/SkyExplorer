2d8a076db9b5953b7d5f8fc5ad88ed8d43903730
Colin <colinschepers@hotmail.com>
2021-12-28 21:33:02 +0100

    Add more filters

--- sky_explorer/streamlit/dashboard.py
+++ sky_explorer/streamlit/dashboard.py
@@ -22,55 +22,71 @@ def get_states():
 class Dashboard:
     def __init__(self):
         self._st_dataframe = None
-        self._callsign = st.session_state.get("callsign", None)
-        self._origin_countries = st.session_state.get("origin_countries", [])
-        self._should_update_continuously = st.session_state.get("should_update_continuously", True)
-        self._map_style = st.session_state.get("map_style", None)
         self._map_renderer = MapRenderer()
+        self._callsign = None
+        self._origin_countries = None
+        self._longitude = None
+        self._latitude = None
+        self._altitude = None
+        self._velocity = None
 
     def run(self):
         states = get_states()
-        self.render_sidebar(states)
 
-        st.title("Sky Explorer")
-        self._st_dataframe = st.empty()
-        self._map_renderer.draw()
-
-        if self._should_update_continuously:
-            asyncio.run(self._update_continuously())
-        else:
-            self._update(states)
-
-    def render_sidebar(self, states: pd.DataFrame):
         with st.sidebar:
-            st.subheader("Filters")
+            st.header("Filters")
             self._callsign = st.text_input(label="Callsign", value="", max_chars=8, key="callsign").upper()
             self._origin_countries = set(st.multiselect(
                 label="Country of origin",
                 options=states["origin_country"].sort_values().unique(),
                 key="origin_countries"
             ))
+            self._longitude = st.slider(label="Longitude (decimal degrees)", min_value=-180, max_value=180,
+                                        value=(-180, 1890), step=1, key="longitude")
+            self._latitude = st.slider(label="Latitude (decimal degrees)", min_value=-90, max_value=90,
+                                       value=(-90, 90), step=1, key="latitude")
+            self._altitude = st.slider(label="Altitude (meters)", min_value=0, max_value=30000,
+                                       value=(0, 30000), step=100, key="altitude")
+            self._velocity = st.slider(label="Velocity (m/s)", min_value=0, max_value=500,
+                                       value=(0, 500), step=10, key="velocity")
+            self._azimuth = st.slider(label="Azimuth (decimal degrees)", min_value=0, max_value=360,
+                                      value=(0, 360), step=1, key="azimuth")
 
-            st.subheader("Settings")
-            self._should_update_continuously = st.checkbox(
-                label="Update continuously",
+            st.header("Settings")
+            live_view = st.checkbox(
+                label="Live view",
                 value=True,
-                key="should_update_continuously"
+                key="live_view"
             )
 
-            self._map_style = st.selectbox(
+            map_style = st.selectbox(
                 label="Map style",
                 options=MapStyle,
                 format_func=attrgetter('name'),
                 key="map_style"
             )
-            self._map_renderer.set_style(self._map_style)
+
+        st.title("Sky Explorer")
+        self._st_dataframe = st.empty()
+        self._map_renderer.draw(map_style)
+
+        if live_view:
+            asyncio.run(self._update_continuously())
+        else:
+            self._update(states)
 
     def _update(self, states: pd.DataFrame):
+        mask = (states['longitude'].between(*self._longitude)) & \
+               (states['latitude'].between(*self._latitude)) & \
+               (states['baro_altitude'].between(*self._altitude)) & \
+               (states['velocity'].between(*self._velocity)) & \
+               (states['azimuth'].between(*self._azimuth))
         if self._callsign:
-            states = states[states['callsign'].str.contains(self._callsign)]
+            mask &= states['callsign'].str.contains(self._callsign)
         if self._origin_countries:
-            states = states[states['origin_country'].isin(self._origin_countries)]
+            mask &= states['origin_country'].isin(self._origin_countries)
+
+        states = states[mask]
         self._st_dataframe.dataframe(states)
         self._map_renderer.update(states)
 
@@ -79,6 +95,6 @@ class Dashboard:
         while True:
             self._update(get_states())
             for _ in range(CONFIG["refresh_delay"]):
-                # Allow this thread to be killed
+                # Allow this thread to be killed by streamlit by calling streamlit code
                 placeholder.empty()
                 await asyncio.sleep(1)
--- sky_explorer/streamlit/map.py
+++ sky_explorer/streamlit/map.py
@@ -24,7 +24,7 @@ class MapRenderer:
             get_icon="icon_data",
             get_size=1,
             size_scale=25,
-            get_angle="azimuth",
+            get_angle="angle",
             get_position=["longitude", "latitude"],
             pickable=True
         )
@@ -36,15 +36,13 @@ class MapRenderer:
         )
         self._map = None
 
-    def set_style(self, map_style: MapStyle):
+    def draw(self, map_style: MapStyle):
         self._deck.map_style = f"mapbox://styles/mapbox/{map_style.value}"
-        self._deck.update()
-
-    def draw(self):
         self._map = st.pydeck_chart(self._deck)
 
     def update(self, states: pd.DataFrame):
         df = states.assign(icon_data=[self._icon_data] * len(states.index))
-        self._icon_layer.data = df
+        df["angle"] = (360 - df["azimuth"])
+        self._icon_layer.data = df[["callsign", "longitude", "latitude", "angle", "icon_data"]]
         self._deck.update()
         self._map.pydeck_chart(self._deck)
